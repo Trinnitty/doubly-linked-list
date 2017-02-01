d4d2e0cedcc64a0602e163e49a6da8f2141458ef
Tatyana Bukzheleva <ledimersedes@yandex.ru>
2017-02-01 16:51:25 +0300

    C:/Program Files/Git/doubly-linked-list/some change

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,23 +1,83 @@
 const Node = require('./node');
 
+
 class LinkedList {
-    constructor() {}
+    constructor() {
 
-    append(data) {}
+    	this.length = 0;
+    	this._head = null;
+    	this._tail = null;
 
-    head() {}
+    }
 
-    tail() {}
+    append(data) {
 
-    at(index) {}
+    	var node = new Node(data);
 
-    insertAt(index, data) {}
+    	if(this.length == 0){
+    		this._head = node;
+    		this._tail = node;
+    		
+    	}else{
+    		node.prev = this._tail;
+    		this._tail.next = node;
+    		this._tail = node;
+    	}
+    	
+    	this.length++;
+    	return node;
+    }
 
-    isEmpty() {}
+    head() {
+    		return this._head.data;
+    	}
 
-    clear() {}
+    tail() {
+    	return this._tail.data;
+    	}
 
-    deleteAt(index) {}
+    at(index) {
+    	if(this.length < index){
+    		alert("Element with this index is not existing.");
+    	}else{
+    		
+    		var i = 0, data;
+    		var node = this._head;
+    		while(i != index){
+    			node = node.next;
+    			i++;
+    		}
+    	return node.data;
+    	}
+    }
+
+    insertAt(index, data) {
+
+		
+
+    }
+
+    isEmpty() {
+
+    	if (this.length){
+    		return false;    		
+    	}else {
+    		return true;
+    	}
+
+    }
+
+    clear() {
+
+    	this._head.data = null;
+    	this._tail.data = null;
+    	this.length = 0;
+
+    }
+
+    deleteAt(index) {
+
+    }
 
     reverse() {}
 
